3a63e1c61f4a39607a59d1d955df16a8a46fa7e8
tommyrachmadiono <tommyrachmadiono@gmail.com>
2021-12-30 23:18:18 +0700

    Update fillable + relation on model user and role

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'nama', 'email', 'password', 'alamat', 'no_telp', 'prodi', 'role_id'
     ];
 
     /**
@@ -41,4 +39,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id', 'id');
+    }
 }
