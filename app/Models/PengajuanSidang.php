33760e4ba1189f50039f8044b9529e8eca2ab510
tommyrachmadiono <tommyrachmadiono@gmail.com>
2022-01-01 22:38:42 +0700

    Update model pengajuan sidang

---
+++ app/Models/PengajuanSidang.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PengajuanSidang extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pengajuan_sidang';
+
+    protected $fillable = [
+        'id_mahasiswa', 'tanggal_daftar_sidang', 'status_pengajuan'
+    ];
+}
